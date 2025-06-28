ecaa111304b1cb9593c367ab629eaff035f10f86
Zaid Rakhange <zaidcommits.github@gmail.com>
2025-06-28 12:09:36 +0530

    deployment test

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -11,8 +11,7 @@ let client
 let clientPromise: Promise<MongoClient>
 
 if (process.env.NODE_ENV === "development") {
-  // In development mode, use a global variable so that the value
-  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+
   let globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>
   }
